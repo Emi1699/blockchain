216111c3b94deed49585998c2843ab04e90e61fe
Fanel Buliga <febuliga1@sheffield.ac.uk>
2021-02-17 13:41:23 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,77 +1,18 @@
-const SHA256 = require('crypto-js/sha256');
-
-class Block {
-	constructor(index, timestamp, data, previousHash = '') {
-		this.index = index;
-		this.timestamp = timestamp;
-		this.data = data;
-		this.previousHash = previousHash;
-		this.hash = this.calculateHash();
-		this.nonce = 0;
-	}
-
-	calculateHash() {
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-	}
-
-	mineBlock(difficulty) {
-		while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-			this.nonce++;
-			this.hash = this.calculateHash(); 
-			console.log(this.hash)
-		}
-
-		console.log("Block mined:" + this.hash)
-	}
-}
-
-class Blockchain {
-	constructor() {
-		this.chain = [this.createGenesisBlock()];
-		this.difficulty = 3;
-	}
-
-	createGenesisBlock() {
-		return new Block(0, '01/01/2017', "Genesis block", "0");
-	}
-
-	getLatestBlock() {
-		return this.chain[this.chain.length - 1]
-	}
-
-	addBlock(newBlock) {
-		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.mineBlock(this.difficulty)
-		this.chain.push(newBlock);
-	}
-
-	isChainValid() {
-		for (let i = 1; i < this.chain.length; i++) {
-			const currentBlock = this.chain[i];
-			const previousBlock = this.chain[i - 1];
-
-			if (currentBlock.hash !== currentBlock.calculateHash()) {
-				return false;
-			}
-
-			if (currentBlock.previousHash !== previousBlock.hash) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-}
+const {Blockchain, Transaction} = require('./blockchain');
 
 let emiCoin = new Blockchain();
 
-console.log("Mining block 1...")
-emiCoin.addBlock(new Block(1, "17/02/2020", {amount:10}));
-
-console.log("Mining block 2 ...")
-emiCoin.addBlock(new Block(2, "19/02/2020", {amount:4}));
+emiCoin.createTransaction(new Transaction('address 1', 'address 2', 100))
+emiCoin.createTransaction(new Transaction('address 2', 'address 1', 50))
 
 
+console.log('\n Starting the miner...');
+emiCoin.minePendingTransaction('emi address');
+console.log("\nBalance of Emi is ", emiCoin.getBalanceOfAddress("emi address"));
+
+console.log('\n Starting the miner again...');
+emiCoin.minePendingTransaction('emi address');
+console.log("\nBalance of Emi is ", emiCoin.getBalanceOfAddress("emi address"));
 
 
 
